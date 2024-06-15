08cb4296adc353f9c196a905b47ae0d3b488f700
atemengue <moafembe@gmail.com>
2024-06-15 15:26:15 +0100

    update structure of code

--- main.py
+++ main.py
@@ -1,42 +1,43 @@
 from mergeSort import merge_sort
 from  quickSort import quickSort
-from utils.utils import read_csv_and_convert_to_dict, write_csv
+from utils.utils import read_csv_and_convert_to_dict, write_csv, menu
+import time
 
 sorted_tabs= []
 
 def main():
-    path_file = "C:\\Users\\Regis\\Desktop\\Cecile\\thema.csv"
-
-    print("-----------Algorithme de tri  ---------")
-    print("Choississez votre algorithme")
-    print("Tapez 1: Quick Sort")
-    print("Tapez 2: Merge Sort")
-
-    choix = input("Faites votre choix: ")
-
+    Trouve = False
+    path_file = input("Entrer le chemin du fichier: ")
+    start = 0
+    end = 0
     # lecture et transformation en tableau de dictionnaire
+
     array_dict = read_csv_and_convert_to_dict(path_file)
+
+    menu()
+
+    while (not Trouve):
+        choix = input("Choississez 1 ou 2: ")
+        if choix == '1' or choix == '2':
+            Trouve = True
 
     # trie avec le quick ou le merge
     if choix == '1':
+        start = time.time()
+
         sorted_tabs = quickSort(array_dict)
+
+        end = time.time()
     elif choix == '2':
+        start = time.time()
+
         sorted_tabs = merge_sort(array_dict)
-    else:
-        print("Veuillez choisir 1 ou 2")
-        return 0
+
+        end = time.time()
 
     # creation du fichier csv trie
     write_csv(sorted_tabs)
+    print(f'Time Execution:", {(end - start) * 1000} milliseconds')
 
 #Appel du main
 main()
-
-#A   B
-
-# # [ {
-#     "D1" : 6565
-# # [{
-# #     "Inventarort": "D1"
-# #     "Inventarnummer" : 586
-# # }]
