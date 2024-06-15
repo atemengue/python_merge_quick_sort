58e20d0adf69bb91bc641030088beeee3d94b015
atemengue <moafembe@gmail.com>
2024-06-15 11:56:19 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import csv
+
+
+HEADER = "ï»¿Inventarnummer;Inventarort"
+array_dict = []
+def main():
+    path_file = "C:\\Users\\Regis\\Desktop\\Cecile\\thema.csv"
+
+    with open(path_file, 'r') as csvfile:
+        csv_dic_reader = csv.DictReader(csvfile)
+
+        for row in csv_dic_reader:
+            value = row[HEADER]
+            inventarort, inventarnummer = value.split(";")
+            array_dict.append({inventarnummer: int(inventarort)})
+
+#Appel du main
+main()
